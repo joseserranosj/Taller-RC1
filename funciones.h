1395a709beb8e7489f59859951bfd2d423f0b6f6
joseserranosj <jose.serrano.jaramillo@udla.edu.ec>
2025-11-18 01:42:34 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,23 +1,12 @@
-// funciones.h
-#ifndef FUNCIONES_H
-#define FUNCIONES_H
-
-#define MAX 10
-#define MAX_NOMBRE 20
-
-// Prototipos
-void ingresarProducto(char nombres[][MAX_NOMBRE], float tiempos[], int recursos[], int cantidades[], int *cont);
-
-int buscarProducto(char nombres[][MAX_NOMBRE], int cont, char buscado[]);
-
-void editarProducto(char nombres[][MAX_NOMBRE], float tiempos[], int recursos[], int cantidades[], int cont);
-
-void eliminarProducto(char nombres[][MAX_NOMBRE], float tiempos[], int recursos[], int cantidades[], int *cont);
-
-float calcularTiempoTotal(float tiempos[], int cantidades[], int cont);
-
-int calcularRecursosTotales(int recursos[], int cantidades[], int cont);
-
-int verificarCapacidad(float tiempoTotal, float tiempoDisponible, int recursosTotales, int recursosDisponibles);
-
-#endif
+int Menu();
+void SaltodeLinea(char *cadena, int n);
+void MostrarInventario(int *pantallas, int *baterias, int *chips, int *tarjetagrafica, int *teclados, int *mouses);
+void RealizarPedido(char nombreproducto[5][50], int contador, int *pantallas, int *teclados,
+                    int *mouses, int *baterias, int *chips, int *tarjetagrafica, int opcionproducto, int cantidadpedido, int respuestaRP);
+void EditarProducto(int *pantallas, int *teclados, int *mouses, int *baterias, int *chips, int *tarjetagrafica, int respuestaEP);
+int BuscarProducto(char nombreproducto[5][50], int *pantallas, int *teclados, int *mouses, int *baterias, int *chips, int *tarjetagrafica, int contador);
+void EliminarProducto(char nombreproducto[5][50], int *pantallas, int *teclados, int *mouses,
+                      int *baterias, int *chips, int *tarjetagrafica, int *contador, int respuestaEliminar);
+void Reabastecerstock(int *pantallas, int *teclados, int *mouses, int *tarjetagrafica, int *baterias, int *chips, int respuestaR, int opcionesR);
+void AgregarProducto(char nombreproducto[5][50], int *pantallas, int *teclados, int *mouses,
+                     int *baterias, int *chips, int *tarjetagrafica, int *contador);
